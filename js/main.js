fdd1829c9b4f825f17cb341693171831a0ad0012
pokiza1228 <baxromovapokiza1@gmail.com>
2022-03-30 23:25:23 +0500

    o'xshamayabdi

--- js/main.js
+++ js/main.js
@@ -1,8 +1,40 @@
-const cretedElement = function(elName, className, textContant) {
+let cretElement = function(elName, className, textContant, id, src) {
     const element = document.createElement(elName);
-    element.className = className;
+
+    if (className) {
+        element.className = className;
+    }
+
+    if (id) {
+        element.id = id;
+    }
+
     if (textContant) {
         element.textContant = textContant
     }
-    return cretedElement;
+
+    if (src) {
+        element.src = src;
+    }
+
+    return cretElement;
+}
+const productWrapper = document.querySelector(".wrapper");
+const productList = cretElement("ul", "product");
+productWrapper.append(productList);
+
+for (let i = 0; i <= products.length; i++) {
+    const product = products[i];
+
+    const item = cretElement("li", "product__item", product.title, product.id);
+
+    const productImg = document.createElement("img");
+    productImg.src = product.img;
+
+
+    item.append(productImg);
+
+    productList.append(item);
+
+
 }
