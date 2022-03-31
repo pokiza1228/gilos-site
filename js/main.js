617b3a57d086e3fa57d673cd803ea94ebf93308e
pokiza1228 <baxromovapokiza1@gmail.com>
2022-03-31 12:39:52 +0500

    ul product list over

--- js/main.js
+++ js/main.js
@@ -45,7 +45,26 @@ for (let i = 0; i < products.length; i++) {
 
     const benifitsList = createElement("ul", "d-flex flex-wrap list-unstyled");
 
-    //for (let j = 0; j < product.benefits.length; j++) 
+    for (let j = 0; j < product.benefits.length; j++) {
+        const benifits = product.benefits[j];
+
+        const benifitsItem = createElement("li", "badge bg-primary me-1 mb-1", benifits);
+
+        benifitsList.append(benifitsItem);
+
+    }
+
+    const buttonDiv = createElement("div", "position-absolute top-0 end-0 d-flex");
+
+    const button1 = createElement("button", "btn rounded-0 btn-secondary");
+    const button1i = createElement("i", "fa-solid fa-pen");
+    button1.append(button1i);
+    buttonDiv.append(button1);
+
+    const button2 = createElement("button", "btn rounded-0 btn-danger");
+    const button2i = createElement("i", "fa-solid fa-trash");
+    button2.append(button2i);
+    buttonDiv.append(button2);
 
     productWrapper.append(item);
     item.append(wrapper);
@@ -57,6 +76,7 @@ for (let i = 0; i < products.length; i++) {
     cardText.append(productModel);
     cardText.append(productDate);
     cardText.append(benifitsList);
+    cardText.append(buttonDiv);
 
 
 }
