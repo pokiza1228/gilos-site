f68a852c6e26253a924a2e3d6b5bd7af65cabbab
pokiza1228 <baxromovapokiza1@gmail.com>
2022-03-31 09:16:19 +0500

    2

--- js/main.js
+++ js/main.js
@@ -11,30 +11,40 @@ const createElement = function(elName, className, textContent) {
 
 
 const productWrapper = document.querySelector(".wrapper");
-const productList = createElement("ul", "product");
+const productList = createElement("ul", "row list-unstyled g-3");
 
 
 for (let i = 0; i < products.length; i++) {
     const product = products[i];
 
-    const item = createElement("li", "product__item");
+    const item = createElement("li", "col-4");
     item.id = product.id;
-    const productTitle = createElement("h2", "product__title", product.title);
 
+    const wrapper = createElement("div", "card")
 
     const productImg = createElement("img", "product__img");
     productImg.src = product.img;
 
 
-    const productPrice = createElement("p", "product__price", `Price: ${product.price}`);
+    const cardText = createElement("div", "card-body");
 
-    const productModel = createElement("p", "product__model", `Model: ${product.model}`);
+
+    const productTitle = createElement("h3", "card-title", product.title);
+
+    const x = product.price / 4;
+
+    const productPrice = createElement("p", "card-text fw-bold");
+    const productMark = createElement("mark", "", x);
+    productPrice.append(productMark);
+
+    const productModel = createElement("p", "product__model", product.model);
 
     item.append(productTitle);
     item.append(productImg);
     item.append(productPrice);
     item.append(productModel);
     productList.append(item);
+
 
 
 }
